41775fcaeb42797da88283c800a6a54fe6003389
Віталій Карпів <vitalijk@MacBook-Pro-Vitalij.local>
2022-01-30 15:19:11 +0200

    add possibility to add hardcored answers

---
+++ 8-Ball/FirestoreManager.swift
@@ -0,0 +1,31 @@
+//
+//  FirestoreManager.swift
+//  8-Ball
+//
+//  Created by Віталій Карпів on 29.01.2022.
+//
+
+import Foundation
+import Firebase
+
+struct FirestoreManager{
+    var db = Firestore.firestore()
+    
+    func saveAnswerIntoFirestore(_ textField: UITextField) {
+        if let saveData = textField.text {
+            db.collection("answers").addDocument(data: [
+                "answer": saveData
+            ]) { err in
+                if let _ = err {
+                    textField.text = ""
+                    textField.placeholder = "Something went wrong"
+                    textField.backgroundColor = UIColor.red
+                } else {
+                    textField.text = ""
+                }
+            }
+        } else {
+            textField.placeholder = "Type something"
+        }
+    }
+}
--- 8-Ball/SettingsViewController.swift
+++ 8-Ball/SettingsViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var db = Firestore.firestore()
+    let idCell = "idCell"
+    var firestoreManager = FirestoreManager()
+    var answerList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFieldOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SettingsViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+        loadAnswers()
+    }
+    
+    func loadAnswers() {
+        answerList = []
+        db.collection("answers").getDocuments() { (querySnapshot, err) in
+            if let _ = err {
+                print("Error getting documents")
+            } else {
+                for document in querySnapshot!.documents {
+                    if let saveAnswer = document.data()["answer"] {
+                        self.answerList.append(saveAnswer as! String)
+                        DispatchQueue.main.async {
+                            self.tableViewOutlet.reloadData()
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func goBackPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        firestoreManager.saveAnswerIntoFirestore(textField)
+        loadAnswers()
+        self.tableViewOutlet.reloadData()
+        return true
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answerList.count-1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
+        cell.textLabel?.text = answerList[indexPath.row]
+        return cell
     }
 }
